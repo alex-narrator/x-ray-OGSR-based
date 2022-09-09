4d84c7903c27380de8f052087f6e5a12d6f8d2d1
xer-urg <alex.narrator@gmail.com>
2022-09-09 19:02:14 +0300

    +покращення
    джерело:
    https://github.com/OGSR/OGSR-Engine/commit/8a8a46db30614863f1d356cd2a02702c42453b51
    https://github.com/OGSR/OGSR-Engine/commit/4e793cfbe842e72fb8b029650c03b8c12d8a3d62

--- ogsr_engine/COMMON_AI/script_engine.h
+++ ogsr_engine/COMMON_AI/script_engine.h
@@ -31,45 +31,37 @@ public:
 	virtual						~CScriptEngine() = default;
 	void init();
 	virtual	void				unload();
-	static	int					lua_panic(lua_State *L);
+	static	int					lua_panic(lua_State* L);
 #ifdef LUABIND_NO_EXCEPTIONS
-	static	void				lua_error(lua_State *L);
+	static	void				lua_error(lua_State* L);
 #endif
-	static	int					lua_pcall_failed(lua_State *L);
+	static	int					lua_pcall_failed(lua_State* L);
 	void				setup_auto_load();
 	bool				process_file_if_exists(const char* file_name, bool warn_if_not_exist);
 	bool				process_file(const char* file_name);
 	bool				process_file(const char* file_name, bool reload_modules);
-	bool				function_object(const char* function_to_call, luabind::object &object, int type = LUA_TFUNCTION);
+	bool				function_object(const char* function_to_call, luabind::object& object, int type = LUA_TFUNCTION);
 	void				register_script_classes();
-	void				parse_script_namespace(const char *name, char *ns, u32 nsSize, char *func, u32 funcSize);
+	void				parse_script_namespace(const char* name, char* ns, u32 nsSize, char* func, u32 funcSize);
 
 	void				collect_all_garbage();
 
 	template <typename TResult>
-	IC		bool				functor(const char* function_to_call, luabind::functor<TResult> &lua_function);
+	IC bool functor(const char* function_to_call, luabind::functor<TResult>& lua_function) {
+		luabind::object object;
+		if (!function_object(function_to_call, object))
+			return false;
+		//try {
+		lua_function = luabind::object_cast<luabind::functor<TResult>>(object);
+		//}
+		//catch (...) {
+		//	return false;
+		//}
+		return true;
+	}
 
 	DECLARE_SCRIPT_REGISTER_FUNCTION
 };
 add_to_type_list(CScriptEngine)
 #undef script_type_list
 #define script_type_list save_type_list(CScriptEngine)
-
-template <typename TResult>
-IC bool CScriptEngine::functor(const char* function_to_call, luabind::functor<TResult> &lua_function)
-{
-	luabind::object object;
-	if (!function_object(function_to_call, object))
-		return false;
-	try {
-#ifdef LUABIND_09
-		lua_function = luabind::functor<TResult>(object);
-#else
-		lua_function = luabind::object_cast<luabind::functor<TResult>>(object);
-#endif
-	}
-	catch (...) {
-		return false;
-	}
-	return true;
-}
--- ogsr_engine/xrGame/ui/UIScriptWnd.cpp
+++ ogsr_engine/xrGame/ui/UIScriptWnd.cpp
@@ -4,30 +4,22 @@
 #include "../object_broker.h"
 #include "../callback_info.h"
 
-struct event_comparer{
+struct event_comparer {
 	shared_str			name;
 	s16					event;
 
-	event_comparer(shared_str n, s16 e):name(n),event(e){}
-	bool operator ()(SCallbackInfo* i){
-		return( (i->m_controlName==name) && (i->m_event==event) );
+	event_comparer(shared_str n, s16 e) :name(n), event(e) {}
+	bool operator ()(SCallbackInfo& i) {
+		return((i.m_controlName == name) && (i.m_event == event));
 	}
 };
 
-CUIDialogWndEx::CUIDialogWndEx():inherited()
+CUIDialogWndEx::CUIDialogWndEx() :inherited()
 {
 	Hide();
 }
 
-CUIDialogWndEx::~CUIDialogWndEx()
-{
-	try {
-		delete_data(m_callbacks);
-	}
-	catch(...) {
-	}
-}
-void CUIDialogWndEx::Register			(CUIWindow* pChild)
+void CUIDialogWndEx::Register(CUIWindow* pChild)
 {
 	pChild->SetMessageTarget(this);
 }
@@ -40,16 +32,16 @@ void CUIDialogWndEx::Register(CUIWindow* pChild, LPCSTR name)
 
 void CUIDialogWndEx::SendMessage(CUIWindow* pWnd, s16 msg, void* pData)
 {
-	event_comparer ec(pWnd->WindowName(),msg);
+	event_comparer ec(pWnd->WindowName(), msg);
 
-	CALLBACK_IT it = std::find_if(m_callbacks.begin(),m_callbacks.end(),ec);
-	if(it==m_callbacks.end())
+	CALLBACK_IT it = std::find_if(m_callbacks.begin(), m_callbacks.end(), ec);
+	if (it == m_callbacks.end())
 		return inherited::SendMessage(pWnd, msg, pData);
 
-	((*it)->m_callback)();
+	(*it).m_callback();
 
-//	if ( (*it)->m_cpp_callback )	
-//		(*it)->m_cpp_callback(pData);
+	//	if ( (*it)->m_cpp_callback )	
+	//		(*it)->m_cpp_callback(pData);
 }
 
 bool CUIDialogWndEx::Load(LPCSTR xml_name)
@@ -57,34 +49,33 @@ bool CUIDialogWndEx::Load(LPCSTR xml_name)
 	return true;
 }
 
-SCallbackInfo*	CUIDialogWndEx::NewCallback ()
+SCallbackInfo* CUIDialogWndEx::NewCallback()
 {
-	m_callbacks.push_back( xr_new<SCallbackInfo>() );
-	return m_callbacks.back();
+	return &m_callbacks.emplace_back();
 }
 
-void CUIDialogWndEx::AddCallback(LPCSTR control_id, s16 event, const luabind::functor<void> &lua_function)
+void CUIDialogWndEx::AddCallback(LPCSTR control_id, s16 event, const luabind::functor<void>& lua_function)
 {
-	SCallbackInfo* c	= NewCallback ();
-	c->m_callback.set	(lua_function);
-	c->m_controlName	= control_id;
-	c->m_event			= event;
-	
+	SCallbackInfo* c = NewCallback();
+	c->m_callback.set(lua_function);
+	c->m_controlName = control_id;
+	c->m_event = event;
+
 }
 
-void CUIDialogWndEx::AddCallback (LPCSTR control_id, s16 event, const luabind::functor<void> &functor, const luabind::object &object)
+void CUIDialogWndEx::AddCallback(LPCSTR control_id, s16 event, const luabind::functor<void>& functor, const luabind::object& object)
 {
-	SCallbackInfo* c	= NewCallback ();
-	c->m_callback.set	(functor,object);
-	c->m_controlName	= control_id;
-	c->m_event			= event;
+	SCallbackInfo* c = NewCallback();
+	c->m_callback.set(functor, object);
+	c->m_controlName = control_id;
+	c->m_event = event;
 }
 
 
 
 bool CUIDialogWndEx::OnKeyboard(int dik, EUIMessages keyboard_action)
 {
-	return inherited::OnKeyboard(dik,keyboard_action);
+	return inherited::OnKeyboard(dik, keyboard_action);
 }
 void CUIDialogWndEx::Update()
 {
@@ -93,6 +84,6 @@ void CUIDialogWndEx::Update()
 
 
 void CUIDialogWndEx::ClearCallbacks() {
-  for ( auto it = m_callbacks.begin(); it != m_callbacks.end(); ++it )
-    (*it)->m_callback.clear();
+	for (auto& cb : m_callbacks)
+		cb.m_callback.clear();
 }
